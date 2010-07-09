6a373cbaf4c81269087f12ba59632e1a02d47bb7
marius a. eriksen <marius@monkey.org>
2010-07-09 00:05:08 -0400

    cleanup.

--- lib/geohash.ml
+++ lib/geohash.ml
@@ -1,7 +1,5 @@
 open Batteries
 
-(* TODO TODO use bitsets. *)
-
 (* base32 alphabet *)
 let charset = "0123456789bcdefghjkmnpqrstuvwxyz"
 (* TODO: this could even be expanded to the bitstrings directly. *)
@@ -15,38 +13,12 @@ let charset_reverse =
   done;
   a
 
-(* up to 32. *)
-let bits_of_int i =
+let bits_of_b32 i =
   [ i land 16 == 16;
     i land  8 ==  8;
     i land  4 ==  4;
     i land  2 ==  2;
     i land  1 ==  1 ]
-
-let rec chunk_by n xs =
-  match List.split_at n xs with
-      ([], []) -> [[]]
-    | (chunk, []) -> [chunk]
-    | (chunk, rest) -> chunk :: chunk_by n rest
-
-(* XXX - do the same (given that we're only dealing with 5 bits anyway.) *)
-
-let rec int_of_bits = function
-  | true  :: rest -> (1 lsl (List.length rest)) lor int_of_bits rest
-  | false :: rest -> int_of_bits rest
-  | [] -> 0
-
-(* Generate a bitstring using the given range and length. *)
-let rec bitstring (a, b) value length = 
-  if length == 0
-  then []
-  else begin
-    let mid = a +. ((b -. a) /. 2.) in
-    let length' = length - 1 in
-    if value > mid
-    then  true :: bitstring (mid, b) value length'
-    else false :: bitstring (a, mid) value length'
-  end
 
 let bitstring_of_float (a, b) value =
   Enum.from_loop (a, b) begin fun (a, b) ->
@@ -78,13 +50,6 @@ let interleave x y =
       | None -> raise Enum.No_more_elements
   end
 
-let rec unbitstring (a, b) = function
-  | x :: xs ->
-    let mid = a +. ((b -. a) /. 2.) in
-    if x then unbitstring (mid, b) xs
-    else      unbitstring (a, mid) xs
-  | [] -> (a, b)
-
 let deinterleave xs =
   let rec go xs ys zs =
     match xs with
@@ -92,6 +57,13 @@ let deinterleave xs =
       | [] -> (List.rev ys, List.rev zs) 
       | _ -> invalid_arg "list not of even length!" in
   go xs [] []
+
+let rec unbitstring (a, b) = function
+  | x :: xs ->
+    let mid = a +. ((b -. a) /. 2.) in
+    if x then unbitstring (mid, b) xs
+    else      unbitstring (a, mid) xs
+  | [] -> (a, b)
 
 let of_lat_lon ?(prec = 10) (lat, lon) =
   let lonbits = bitstring_of_float (-180., 180.) lon in
@@ -103,10 +75,7 @@ let of_lat_lon ?(prec = 10) (lat, lon) =
 let parse gh =
   let l = String.to_list gh in
   let vals = List.map (fun c -> charset_reverse.(Char.code c)) l in
-  let bitss = List.map bits_of_int vals in
+  let bitss = List.map bits_of_b32 vals in
   let bits = List.concat bitss in
   let (lon_bits, lat_bits) = deinterleave bits in
   (unbitstring (-90., 90.) lat_bits, unbitstring (-180., 180.) lon_bits)
-
-
-(* give it setters and getters. *)
